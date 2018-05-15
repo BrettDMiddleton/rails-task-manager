f167cd550b70d167a016d6220fbdcf8cbfb4be70
Brett Middleton <brett01101101@gmail.com>
2018-05-15 15:24:19 -0400

    complete, needs checks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  resources :tasks
+
+  # READ a list of tasks
+  get "tasks", to: "tasks#index"
+  # READ a indiviudal task
+  get "tasks/:id", to: "tasks#show"
+  # CREATE a new task
+  post "tasks", to: "tasks#new"
+  # UPDATE. Get a form
+  get "tasks/:id/update", to: "tasks#edit"
+  # UPDATE
+  patch "tasks/:id", to: "tasks#update"
+  # DESTROY a given task
+  delete "tasks/:id", to: "tasks#destroy"
+
+  # CREATE. Get a form
+  # task#new
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_142957) do
+ActiveRecord::Schema.define(version: 2018_05_15_144307) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
